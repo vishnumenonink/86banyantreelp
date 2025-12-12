96ea2b66375807046b8f7775256e99f19b141782
Ink Media <inkmedia@Inks-MacBook-Air.local>
2025-12-12 12:55:50 +0530

    updated thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -54,13 +54,13 @@ export default function ThankYouPage() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center p-6">
-      <h1 className="text-3xl md:text-5xl font-semibold text-amber-700">
+      <h1 className="text-xl md:text-4xl font-semibold text-gray-500">
         Thank you! We’ll get in touch soon.
       </h1>
 
       <Link
         href="/"
-        className="mt-8 text-green-900 font-medium hover:underline text-lg"
+        className="mt-8 text-gray-900 font-medium hover:underline text-lg"
       >
         Go back to Home
       </Link>
